ed5ecca197466423a3a418f671d5d06cc9e95f5c
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-26 00:13:00 +0700

    Enhance shell script for Apache and MariaDB setup
    Updated Apache installation command to use --noconfirm and added MariaDB installation and database setup.

--- slims-shellscript.sh
+++ slims-shellscript.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 #install apache
-sudo pacman -S apache &&
+sudo pacman -S apache --noconfirm &&
 sudo systemctl enable httpd &&
-sudo systemctl start httpd
+sudo systemctl start httpd &&
+
+# install mariadb
+sudo pacman -S mariadb php php-fpm php-gd --noconfirm &&
+sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql &&
+sudo systemctl enable mariadb &&
+sudo systemctl start mariadb &&
+
+# create database & user
+sudo mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS slims;
+CREATE USER IF NOT EXISTS 'slims_user'@'localhost'
+IDENTIFIED BY 'userslims';
+GRANT ALL PRIVILEGES ON slims.* TO 'slims_user'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
