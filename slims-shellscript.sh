272289226050bdfe33bfc8cd4f84b82a1d27f9f2
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-26 01:01:45 +0700

    update

--- slims-shellscript.sh
+++ slims-shellscript.sh
@@ -23,4 +23,45 @@ EOF
 cd /home/null/Downloads &&
 wget https://github.com/slims/slims9_bulian/releases/download/v9.7.2/slims9_bulian-9.7.2.tar.gz &&
 sudo tar -xf /home/null/Downloads/slims9_bulian-9.7.2.tar.gz -C /srv/http &&
-sudo mv /srv/http/slims9_bulian-9.7.2 /srv/http/slims
+sudo mv /srv/http/slims9_bulian-9.7.2 /srv/http/slims &&
+sudo chown -R http:http /srv/http/slims &&
+
+# CONFIGURATION APACHE
+APACHE_CONF="/etc/httpd/conf/httpd.conf"
+
+# BACKUP FILE KONFIGURASI
+sudo cp $APACHE_CONF ${APACHE_CONF}.bak
+
+# ENABLE REQUIRED MODULES
+sudo sed -i \
+-e 's/^#LoadModule rewrite_module/LoadModule rewrite_module/' \
+$APACHE_CONF
+
+# Tambahkan proxy & proxy_fcgi setelah AddHandler php
+sudo sed -i '/AddHandler php-script .php/a \
+LoadModule proxy_module modules/mod_proxy.so\n\
+LoadModule proxy_fcgi_module modules/mod_proxy_fcgi.so' \
+$APACHE_CONF
+
+# ENABLE PHP-FPM HANDLER
+sudo tee -a $APACHE_CONF > /dev/null <<EOF
+
+<FilesMatch "\.php$">
+    SetHandler "proxy:unix:/run/php-fpm/php-fpm.sock|fcgi://localhost/"
+</FilesMatch>
+EOF
+
+# SET AllowOverride All
+sudo sed -i \
+-e 's/AllowOverride None/AllowOverride All/' \
+$APACHE_CONF
+
+# SET DIRECTORY INDEX
+sudo sed -i \
+-e 's/DirectoryIndex index.html/DirectoryIndex index.php index.html index.htm/' \
+$APACHE_CONF
+
+# RESTART APACHE
+sudo systemctl enable php-fpm &&
+sudo systemctl start php-fpm &&
+sudo systemctl restart httpd
