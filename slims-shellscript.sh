902e0f65929c9be78e64dbb36a5519cc6c102738
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-26 02:24:29 +0700

    update

--- slims-shellscript.sh
+++ slims-shellscript.sh
@@ -26,7 +26,7 @@ sudo tar -xf /home/null/Downloads/slims9_bulian-9.7.2.tar.gz -C /srv/http &&
 sudo mv /srv/http/slims9_bulian-9.7.2 /srv/http/slims &&
 sudo chown -R http:http /srv/http/slims &&
 
-# CONFIGURATION APACHE
+# Configuration apache
 APACHE_CONF="/etc/httpd/conf/httpd.conf"
 
 # Backup konfigurasi
@@ -37,3 +37,52 @@ sudo sed -i '/^LoadModule rewrite_module modules\/mod_rewrite.so/a \
 LoadModule proxy_module modules/mod_proxy.so\n\
 LoadModule proxy_fcgi_module modules/mod_proxy_fcgi.so' \
 $APACHE_CONF
+
+# Uncommenting module rewrite
+sudo sed -i \
+-e 's/^#LoadModule rewrite_module/LoadModule rewrite_module/' \
+$APACHE_CONF
+
+# Menambahkan pada baris paling terakhir
+sudo tee -a $APACHE_CONF > /dev/null <<EOF
+
+<FilesMatch "\.php$">
+    SetHandler "proxy:unix:/run/php-fpm/php-fpm.sock|fcgi://localhost/"
+</FilesMatch>
+EOF
+
+# Mengubah aksess override pada direktori /srv/http
+sudo sed -i \
+-e 's/AllowOverride None/AllowOverride All/' \
+$APACHE_CONF
+
+# Menambahkan pada directory index
+sudo sed -i \
+-e 's/DirectoryIndex index.html/DirectoryIndex index.php index.html index.htm/' \
+$APACHE_CONF
+
+PHP_INI="/etc/php/php.ini"
+
+# Backup php.ini
+sudo cp $PHP_INI ${PHP_INI}.bak
+
+# Uncommenting beberapan extension
+sudo sed -i \
+-e 's/^;extension=mysqli/extension=mysqli/' \
+-e 's/^;extension=gettext/extension=gettext/' \
+-e 's/^;extension=pdo_mysql/extension=pdo_mysql/' \
+-e 's/^;extension=gd/extension=gd/' \
+$PHP_INI
+
+# Aktifkan ekstensi xml & mbstring
+sudo sed -i \
+-e 's/^;extension=xml/extension=xml/' \
+-e 's/^;extension=mbstring/extension=mbstring/' \
+$PHP_INI
+
+# Mengaktifkan php service
+sudo systemctl enable php-fpm &&
+sudo systemctl start php-fm
+
+# Restart apache
+sudo systemctl restart httpd
