6b58c6f23b333475358c31a41cefbf4b50965175
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-28 04:50:52 +0700

    update

--- test.sh
+++ test.sh
@@ -6,9 +6,10 @@ apin=$(sudo pacman -Qe | grep apache)
 if [[ -z "$apin" ]]; then
     echo "apache is not exist"
     sudo pacman -S apache --noconfirm
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
 
 else 
     echo "apache already installed"
 fi
+
+    sudo systemctl enable httpd &&
+    sudo systemctl start httpd
