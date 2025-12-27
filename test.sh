80d5ca2c0450317c023e4d1b0363898805543e44
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-28 04:43:00 +0700

    update

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# install apache
 apin=$(sudo pacman -Qe | grep apache)
 
 if [[ -z "$apin" ]]; then
-    echo "apache belum terinstall"
+    echo "apache is not exist"
     sudo pacman -S apache --noconfirm
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+
 else 
-    echo "apache sudah terinstall"
+    echo "apache already installed"
 fi
