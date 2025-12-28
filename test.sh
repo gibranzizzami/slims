fa27d3dc5f1a5b184c222f96f1145c07f2abfb2e
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-28 13:02:04 +0700

    update

--- test.sh
+++ test.sh
@@ -13,3 +13,13 @@ fi
 
     sudo systemctl enable httpd &&
     sudo systemctl start httpd
+
+# install php
+phpin=$(sudo pacman -Qe | grep php php-fpm php-gd)
+
+if [[ -z "$phpin" ]]; then
+    echo "php is not exist"
+    sudo pacman -S php php-fpm php-gd
+else
+    echo "php already installed"
+fi
