4f13a5bb3b166e6af3629636ffa6bb05535a7fd5
gibranzizzami <gbrnzzmi24@gmail.com>
2025-12-29 13:42:24 +0700

    update

--- test.sh
+++ test.sh
@@ -6,20 +6,18 @@ apin=$(sudo pacman -Qe | grep apache)
 if [[ -z "$apin" ]]; then
     echo "apache is not exist"
     sudo pacman -S apache --noconfirm
-
 else 
     echo "apache already installed"
 fi
-
     sudo systemctl enable httpd &&
     sudo systemctl start httpd
 
 # install php
-phpin=$(sudo pacman -Qe | grep php php-fpm php-gd)
+phpin=("php" "php-fpm" "php-gd")
 
 if [[ -z "$phpin" ]]; then
     echo "php is not exist"
-    sudo pacman -S php php-fpm php-gd
+    sudo pacman -S "${PACKAGES[@]}" --noconfirm
 else
-    echo "php already installed"
+    echo "php already exist"
 fi
